138ff2a938cf68b96da44fdb305d0d14fcbc6624
Guilherme <guilhermebarradasdev@gmail.com>
2023-05-31 23:53:18 -0300

    alteraçoes integraçao com bd

--- App.js
+++ App.js
@@ -1,53 +1,145 @@
-import React from 'react';
-import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import telaLogin from './screens/telaLogin';
-import telaCadastro from './screens/telaCadastro';
+
+// Tela de Login
+function TelaLogin({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLoginPress = () => {
+    // Aqui você pode adicionar a lógica para verificar o email e senha na base de dados
+    // Se estiver correto, você pode redirecionar para a tela de conteúdo
+    if (email === localStorage.getItem('email') && password === localStorage.getItem('password')) {
+      navigation.navigate('TelaConteudo');
+    } else {
+      Alert.alert('Credenciais inválidas');
+    }
+  };
+
+  const handleCadastroPress = () => {
+    navigation.navigate('TelaCadastro');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.mainBox}>
+        <Text style={styles.loginTitleText}>Login</Text>
+        <View style={styles.inputBox}>
+          <Text style={styles.inputLabel}>Email</Text>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            keyboardType="email-address"
+            textContentType="emailAddress"
+            value={email}
+            onChangeText={setEmail}
+          />
+        </View>
+        <View style={styles.inputBox}>
+          <Text style={styles.inputLabel}>Senha</Text>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            secureTextEntry={true}
+            textContentType="password"
+            value={password}
+            onChangeText={setPassword}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.loginButton} onPress={handleLoginPress}>
+          <Text style={styles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.cadastroButton} onPress={handleCadastroPress}>
+          <Text style={styles.cadastroButtonText}>Cadastro</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.forgotPasswordText}>Esqueceu a senha?</Text>
+      </View>
+    </View>
+  );
+}
+
+// Tela de Cadastro
+function TelaCadastro({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleCadastroPress = () => {
+    if (email && password) {
+      try {
+        // Salvar os dados localmente (simulando armazenamento)
+        localStorage.setItem('email', email);
+        localStorage.setItem('password', password);
+
+        // Exibir alerta de sucesso e redirecionar para a tela de login
+        Alert.alert('Cadastro efetuado com sucesso');
+        navigation.navigate('TelaLogin');
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      // Exibir mensagem de erro se os campos não estiverem preenchidos
+      Alert.alert('Preencha todos os campos');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.mainBox}>
+        <Text style={styles.loginTitleText}>Cadastro</Text>
+        <View style={styles.inputBox}>
+          <Text style={styles.inputLabel}>Email</Text>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            keyboardType="email-address"
+            textContentType="emailAddress"
+            value={email}
+            onChangeText={setEmail}
+          />
+        </View>
+        <View style={styles.inputBox}>
+          <Text style={styles.inputLabel}>Senha</Text>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            secureTextEntry={true}
+            textContentType="password"
+            value={password}
+            onChangeText={setPassword}
+          />
+        </View>
+        <TouchableOpacity style={styles.cadastroButton} onPress={handleCadastroPress}>
+          <Text style={styles.cadastroButtonText}>Cadastrar</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+// Tela de Conteúdo
+function TelaConteudo() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.conteudoText}>Você está logado</Text>
+    </View>
+  );
+}
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={telaLogin} />
-        <Stack.Screen name="Cadastro" component={telaCadastro} />
+      <Stack.Navigator>
+        <Stack.Screen name="TelaLogin" component={TelaLogin} />
+        <Stack.Screen name="TelaCadastro" component={TelaCadastro} />
+        <Stack.Screen name="TelaConteudo" component={TelaConteudo} />
       </Stack.Navigator>
     </NavigationContainer>
-    // <View style={styles.container}>
-    //   <View style={styles.mainBox}>
-    //     <Text style={styles.loginTitleText}>Login</Text>
-    //     <View style={styles.inputBox}>
-    //       <Text style={styles.inputLabel}>Email</Text>
-    //       <TextInput
-    //         style={styles.input}
-    //         autoCapitalize="none"
-    //         keyboardType="email-address"
-    //         textContentType="emailAddress"
-    //       />
-    //     </View>
-    //     <View style={styles.inputBox}>
-    //       <Text style={styles.inputLabel}>Senha</Text>
-    //       <TextInput
-    //         style={styles.input}
-    //         autoCapitalize="none"
-    //         secureTextEntry={true}
-    //         textContentType="password"
-    //       />
-    //     </View>
-
-    //     <TouchableOpacity style={styles.loginButton}>
-    //       <Text style={styles.loginButtonText}>Login</Text>
-    //     </TouchableOpacity>
-    //     <TouchableOpacity style={styles.cadastroButton}>
-    //       <Text style={styles.cadastroButtonText}>Cadastro</Text>
-    //     </TouchableOpacity>
-    //       <Text style={styles.cadastroButtonText}>Cadastro</Text>
-
-    //     <Text style={styles.forgotPasswordText}>Esqueceu a senha?</Text>
-    //   </View>
-    // </View>
   );
 }
 
@@ -98,13 +190,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
-   cadastroButton: {
+  cadastroButton: {
     backgroundColor: '#7851a9',
     marginTop: 10,
     paddingVertical: 10,
     borderRadius: 4,
   },
-   cadastroButtonText: {
+  cadastroButtonText: {
     color: '#fff',
     textAlign: 'center',
     fontSize: 20,
@@ -114,5 +206,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginTop: 4,
     fontSize: 16,
+  },
+  conteudoText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
